a3198c376942431a12b880326e3281760d0f03c2
Diamondz-coding <61663747+Diamondz-coding@users.noreply.github.com>
2020-03-10 07:32:15 +0000

    Added KEY Files
    Added The Foundations For The Bot!

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.XmdAbg.Xc0-ME9dfi-IqVDCfN9csNXWNq8';
+
+bot.login(process.env.token);
