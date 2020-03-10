aa48648443b5b94c7a2df6f4f60c2fb10a72587e
Diamondz-coding <61663747+Diamondz-coding@users.noreply.github.com>
2020-03-10 08:00:33 +0000

    Added A TON Of New Commands!

--- index.js
+++ index.js
@@ -3,4 +3,42 @@ const bot = new Discord.Client();
 
 const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.XmdAbg.Xc0-ME9dfi-IqVDCfN9csNXWNq8';
 
+var PREFIX = '?';
+
+
+
+bot.on('ready', () =>{
+    console.log('This Bot Is Online!')
+})
+
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(' ');
+
+    switch(args[0]){
+
+      case 'about':
+      message.channel.send("Hi, I'm Wadia V3! I Was Created By A Few People And Whooosh Now I'm Here In Your Server! ")
+      break;
+
+
+      case 'clear':
+      if(!args[1]){
+        message.reply("What Should I Delete?")
+      }
+      message.channel.bulkDelete(args[1]);
+      break;
+
+      case 'CLEAR':
+      if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134"')
+      message.channel.bulkDelete(args[1]);
+      break;
+
+
+
+
+
+
+
+
 bot.login(process.env.token);
